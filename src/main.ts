b35c3b9cd449831135698f6855f433213a8e3e87
hoshi <falloftroy2000@yahoo.co.jp>
2019-07-20 20:13:07 +0900

    初期アップ

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import Vue from 'vue';
+import './plugins/vuetify'
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import firebase from 'firebase';
+
 
 Vue.config.productionTip = false;
+var firebaseConfig = {
+  apiKey: "AIzaSyA-xqXiANGxyMExi7hDwTWSD__QCP_jvd8",
+  authDomain: "my-address-14313.firebaseapp.com",
+  databaseURL: "https://my-address-14313.firebaseio.com",
+  projectId: "my-address-14313",
+  storageBucket: "",
+  messagingSenderId: "968324870920",
+  appId: "1:968324870920:web:25e4874a4ba66ea5"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <v-container text-xs-center justify-center>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <h1>マイアドレス帳</h1>
+        <p>マイアドレス帳をご利用の方は、Googleアカウントでログインしてください。</p>
+      </v-flex>
+
+      <v-flex xs12 mt-5>
+        <v-btn color='info' @click="login">Googleアカウントでログイン</v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { mapActions } from 'vuex';
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+export default {
+  methods: {
+    ...mapActions(['login'])
+  }
+}
 </script>
+
